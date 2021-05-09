d02b0d118256a18c3e69799765c42713f3894cf2
xyz <xyz@DESKTOP-GMU24LV>
2021-05-09 21:58:54 +0530

    ok

--- src/main/java/com/country/addresses/api/service/ExerciseApiCountryAddressesApplication.java
+++ src/main/java/com/country/addresses/api/service/ExerciseApiCountryAddressesApplication.java
@@ -3,11 +3,11 @@ package com.country.addresses.api.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class ExerciseApiCountryAddressesApplication {
 
     public static void main(String[] args) {
-       // SpringApplication.run(ExerciseApiCountryAddressesApplication.class, args);
+        SpringApplication.run(ExerciseApiCountryAddressesApplication.class, args);
     }
 
 }
